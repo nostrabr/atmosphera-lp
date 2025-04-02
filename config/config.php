4ca3f6b53a0844ca5129bbe9536b4200ac413bf0
weslei <wesleicasali18@gmail.com>
2025-04-02 13:18:46 -0300

    start

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// **********AUTOLOAD**********
+require __DIR__.'/../vendor/autoload.php';
+
+// **********VARIÁVEIS DE AMBIENTES**********
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
+
+// **********BASE URL GERAL**********
+$base_url = $_ENV['BASE_URL'];
+
+
+// **********CONTET METATAGS**********
+$titulo_site = $_ENV['TITULO_SITE_SEO'];
+$descricao_site = $_ENV['DESCRICAO_SITE_SEO'];
